432d7bbfa248b2c536fd8f00f377a600ab55650b
kartik <kartik@gypssy.com>
2023-07-06 17:01:42 +0530

    Fixing Error Page

--- src/app/error.js
+++ src/app/error.js
@@ -1,3 +1,4 @@
+'use client'
 const ErrorPage = ()=>{
     return <div className="text-white w-full h-full flex items-center justify-center">
     Sorry , This is page is displayed because there is some error
