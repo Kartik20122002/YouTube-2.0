b2e9df56c85e89288540a1a46967629eeb731192
kartik <kartik@gypssy.com>
2023-07-06 15:06:34 +0530

    refreshed token added

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,12 @@
+import { refreshedToken } from '@/utils/auth/refreshed';
 import { oauth2client } from '@/utils/auth/youtube';
 import { clientId, clientSecret, scopesStr, secret } from '@/utils/secrets/secrets';
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import Google from 'next-auth/providers/google';
 
-const refreshedToken = async (token : any)=>{
+const getNewToken = async (token : any)=>{
+  const newTokens = await refreshedToken(token.refresh_token);
+  token.access_token = newTokens.access_token;
   return token;
 }
 
@@ -25,24 +28,28 @@ const authOptions : NextAuthOptions = {
     secret : secret,
 
     callbacks: {
-      jwt: ({token , account } : any)=> {
+      jwt: async ({token , account } : any)=> {
 
         try {
           
         if (account && account?.access_token) {
           token.access_token = account.access_token;
-          token.expires_at = Date.now() + account.expires_at*1000;
         }
         if(account && account?.refresh_token){
           token.refresh_token = account.refresh_token;
         }
 
-        if(token?.expires_at > Date.now())
-        return token;
+        const access_token_details_res = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${token.access_token}`);
+        const access_token_details = await access_token_details_res.json();
+        const expire_time = access_token_details.expires_in;
 
-        return refreshedToken(token);
+        if(expire_time < 50){
+            return getNewToken(token);
+        }
+        else return token;
+    
         } catch (error) {
-          return refreshedToken(token);
+          return getNewToken(token);
         }
 
       },
--- src/utils/auth/refreshed.ts
+++ src/utils/auth/refreshed.ts
@@ -1,13 +1,14 @@
 import { clientId, clientSecret } from "../secrets/secrets"
 
 export const refreshedToken = async (refreshToken : any)=>{
+
     const url =
     "https://oauth2.googleapis.com/token?" +
     new URLSearchParams({
       client_id: clientId,
       client_secret: clientSecret,
       grant_type: "refresh_token",
-      refresh_token: refreshToken,
+      refresh_token: refreshToken as string,
     } as any)
 
   const response = await fetch(url, {
@@ -19,7 +20,7 @@ export const refreshedToken = async (refreshToken : any)=>{
 
   const refreshedTokens = await response.json()
 
-  return refreshToken;
+  return refreshedTokens;
 
   if (!response.ok) {
     throw refreshedTokens
