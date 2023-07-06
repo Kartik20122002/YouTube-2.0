20c1b630d2a8a2940720b331d9ba5cd11ae5317a
kartik <kartik@gypssy.com>
2023-07-06 16:52:24 +0530

    error page adding

---
+++ src/app/error.js
@@ -0,0 +1,7 @@
+const ErrorPage = ()=>{
+    return <div className="text-white w-full h-full flex items-center justify-center">
+    Sorry , This is page is displayed because there is some error
+    </div>
+}
+
+export default ErrorPage;
