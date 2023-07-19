b00eb2abf3ac8c4f22d080928b2cc75b4048280b
kartik <kartik@gypssy.com>
2023-07-19 20:57:49 +0530

    dfd

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -51,10 +51,8 @@ const authOptions : NextAuthOptions = {
         }
         if (account && account?.refresh_token) {
           token.access_token = account?.refresh_token;
-
-          if(cookieStore.has('TokensForUse') == false){
-            cookieStore.set('TokensForUse' , JSON.stringify(token));
-          }
+          cookieStore.set('TokensForUse' , JSON.stringify(token));
+          
         }
         
         return token;
