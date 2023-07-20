02ee24a40d89d8b12e9c3c70fb1e61719dc933a6
kartik <kartik@gypssy.com>
2023-07-20 15:48:42 +0530

    different try

--- src/app/api/page/popular/route.ts
+++ src/app/api/page/popular/route.ts
@@ -6,26 +6,36 @@ import { NextResponse } from 'next/server'
 import { oauth2client, youtube } from "@/utils/auth/youtube";
 import { signOut } from "next-auth/react";
 import { cookies } from "next/headers";
+import { refreshedToken } from "@/utils/auth/refreshed";
 
 
 export async function GET(req : any ) {
 const cookieStore = cookies();
 
 try{
-
-// if(cookieStore.has('aToken')){
-
-// }
+  
 const tokens = await getToken({req , secret});
 
-if(tokens && tokens?.access_token){ 
-  // const tokenstr = JSON.stringify(tokens?.access_token);
-  const accessToken = tokens?.access_token;
-  const refreshToken = tokens?.refresh_token;
 
-  oauth2client.credentials = {
-    access_token : accessToken as string, 
-    refresh_token : refreshToken as string
+
+if(tokens && tokens?.access_token){ 
+  const accessToken = tokens?.access_token as string;
+  const refreshToken = tokens?.refresh_token as string;
+  const atoken = cookieStore.get('aToken')?.value;
+  
+  if(atoken){
+     oauth2client.credentials = {
+      access_token : atoken,
+      refresh_token : refreshToken as string,
+     }
+  }
+  else{
+    cookieStore.set('aToken',accessToken);
+    console.log('from auth')
+    oauth2client.credentials = {
+      access_token : accessToken as string, 
+      refresh_token : refreshToken as string
+    }
   }
 }
 else{ 
@@ -43,6 +53,35 @@ else{
   chart : 'mostPopular',
   regionCode : 'In',
   });
+
+  if(results.status == 401){
+    console.log('refreshed')
+    const newatoken = await refreshedToken(tokens?.refresh_token);
+    cookieStore.set('aToken',newatoken);
+
+    oauth2client.credentials = {
+      access_token : newatoken,
+      refresh_token : tokens?.refresh_token as string,
+     }
+
+     console.log('new results')
+
+     const newresults = await youtube.videos.list({ 
+      part:['snippet','statistics'], 
+      maxResults : 48,
+      chart : 'mostPopular',
+      regionCode : 'In',
+      });
+
+      if(newresults.status !== 200) {
+        return  NextResponse.json({});
+        }
+        const videos = newresults.data.items;
+        const ptoken = newresults.data.prevPageToken;
+        const ntoken = newresults.data.nextPageToken;
+     
+      return NextResponse.json({videos , ptoken , ntoken});
+  }
 
     if(results.status !== 200) {
     return  NextResponse.json({});
