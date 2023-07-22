d791d15c85cdbd2b1de51582ff87ad0a99f750f1
kartik <kartik@gypssy.com>
2023-07-22 23:02:23 +0530

    Skeleton card bhi ready

--- src/components/searchPage/SearchPage.tsx
+++ src/components/searchPage/SearchPage.tsx
@@ -1,6 +1,8 @@
 import Image from "next/legacy/image";
 import Link from "next/link";
 import {RiPlayListLine} from 'react-icons/ri';
+import SekeltonImg from "../global/skeletonComponents/ImgSkeleton";
+import SekeltonText from "../global/skeletonComponents/TextSkeleton";
 
 const img = 'https://yt3.googleusercontent.com/ytc/AOPolaQygjiMgnSw5zUP1F_PyEkcGBmfaE8HMq7S_xu_=s176-c-k-c0x00ffffff-no-rj';
 const videoImg = 'https://i.ytimg.com/vi/fsNrgCivsZg/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBjTNa2oj9zdcd0gdxGRYylfpzalA'
@@ -9,6 +11,7 @@ const SearchPage = ({query}:any)=>{
     return<>
      <div className="flex flex-col  overflow-y-scroll h-full pb-[3rem] w-full">
         <ChannelCard/>
+        <SkeletonCard/>
         <VideoCard/>
         <PlaylistCard/>
         <VideoCard/>
@@ -40,14 +43,17 @@ const SkeletonCard = ()=>{
     <Link href={''} className="md:w-[90%] md:max-w-[1200px] mx-auto mb-4 dark:text-white flex flex-col md:flex-row w-full">
         <div  className="basis-[25%] shrink-0 md:min-w-[200px] grow">
         <div className="w-full h-full relative overflow-hidden pt-[56.25%]">
-            <Image src={videoImg} className="!absolute md:rounded-lg !min-w-0 !min-h-0 !w-full !h-full top-0 left-0 right-0 bottom-0" layout="fill" alt="videoImg" loading="lazy" />
+            <SekeltonImg className="absolute md:rounded-lg top-0 left-0 bottom-0 right-0"/>
         </div>
         </div>
         <div className="basis-[75%] grow ml-1 md:ml-4 mt-1">
-            <div className="truncate-2 text-[1.1rem] md:w-[70%] font-[400]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit molestias, odit nobis dolores repudiandae optio unde. Numquam sequi nobis ab!, ipsum dolor sit amet consectetur adipisicing elit. Dolore praesentium quia magni, iste deleniti expedita? Eligendi assumenda obcaecati aliquid distinctio.</div>
-            <div className="text-grey text-sm md:w-[70%]">1 day ago</div>
-            <Link href={''} className="text-grey md:w-[70%] dark:hover:text-white hover:text-black text-sm">take u forward</Link>
-            <div className="text-grey text-sm truncate-1 md:w-[70%]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, eveniet nulla beatae qui hic amet asperiores tempore quaerat non error in expedita possimus quae tempora? Unde sed libero et eveniet officiis soluta aliquam? Fugiat commodi nulla hic molestiae obcaecati praesentium.</div>
+            <div className=" md:w-[70%]">
+                <SekeltonText height="min-h-[1.2rem] mb-2"/>
+                <SekeltonText height="min-h-[1.2rem] mb-2"/>
+            </div>
+            <div className="md:w-[70%]"><SekeltonText height="min-h-[0.8rem]" width="min-w-[25%] max-w-[25%]"/></div>
+            <Link href={''} className="text-grey md:w-[70%] dark:hover:text-white hover:text-black text-sm"><SekeltonText height="min-h-[0.8rem]" width="min-w-[25%] max-w-[25%]"/></Link>
+            <div className="text-grey text-sm truncate-1 md:w-[70%]"><SekeltonText height="min-h-[0.8rem]" /></div>
         </div>
     </Link>
     </>
