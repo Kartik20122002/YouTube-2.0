7cbf10cd3b682351cc2e66b352a7ef754a815bd4
kartik <kartik@gypssy.com>
2023-07-23 16:26:07 +0530

    gkjh

--- src/components/global/pagesection/loading.tsx
+++ src/components/global/pagesection/loading.tsx
@@ -9,7 +9,7 @@ import 'react-loading-skeleton/dist/skeleton.css'
 export const VideoContainerSkeleton = ()=>{
 
   return <>
-  <motion.div layout transition={{duration : 0.5}} className= {`px-0 w-full md:w-[19rem] items-center mb-4 flex flex-col justify-evenly cursor-pointer`}>
+  <motion.div layout transition={{duration : 0.5}} className= {`px-0 w-full ${isLarge ? 'md:w-[19rem]' : 'md:w-[21rem]'} items-center mb-4 flex flex-col justify-evenly cursor-pointer`}>
 
         <motion.div className={`flex pt-[56.25%] h-full w-full relative md:rounded-md`} layout transition={{duration : 0.5}}>
           <SkeletonImg className="md:rounded-xl absolute top-0 right-0 h-full w-full"/>
