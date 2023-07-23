86557e747c76084a94108ba19b3793675de17c2c
kartik <kartik@gypssy.com>
2023-07-23 15:09:00 +0530

    asfdsvxc

--- src/components/global/sidebar/Sidebar.tsx
+++ src/components/global/sidebar/Sidebar.tsx
@@ -2,13 +2,14 @@ import Image from "next/legacy/image";
 import Link from "next/link";
 import React, { useContext, useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import { AiFillHome , AiOutlineHome , AiFillLike , AiOutlineLike } from "react-icons/ai";
+import { AiFillHome , AiOutlineHome , AiFillLike , AiOutlineLike, AiOutlineDown } from "react-icons/ai";
 import { MdLibraryAdd , MdOutlineLibraryAdd } from 'react-icons/md';
 import { useSession } from "next-auth/react";
 import { slideContext } from "@/app/layout";
 import Loader from "../loader/Loader";
 import Sekelton from "@/components/global/skeletonComponents/ImgSkeleton";
 import SekeletonTxt from "@/components/global/skeletonComponents/TextSkeleton"
+import { RiPlayList2Line } from "react-icons/ri";
 export const dynamic = 'force-dynamic'
 
 const links = [
@@ -43,6 +44,23 @@ const Sub = ({item , isLarge}:any)=>{
     </motion.div>
 }
 
+const SubSkeleton = ({isLarge} : any)=>{
+  return <motion.div layout transition={{duration : 0.5}} className="subscribed-list w-full pl-[3%] pb-[15%]">
+  {isLarge && 
+   <motion.h3 layout transition={{duration : 0.5}} initial={{opacity : isLarge ? 0 : 1}} animate={{opacity : isLarge ? 1 : 0}} className="text-[18px] dark:text-white ml-2 my-5 text-[#5a5a5a]">Subscriptions</motion.h3>
+  }
+
+ <SubsSkeleton isLarge={isLarge}/>
+ <SubsSkeleton isLarge={isLarge}/>
+ <SubsSkeleton isLarge={isLarge}/>
+ <SubsSkeleton isLarge={isLarge}/>
+ <SubsSkeleton isLarge={isLarge}/>
+ <SubsSkeleton isLarge={isLarge}/>
+ <SubsSkeleton isLarge={isLarge}/>
+  
+</motion.div>
+}
+
 const SideLinks = ({item , isLarge , index} : any) =>{
   const {slide , setslide} = useContext(slideContext) as any;
 
@@ -63,25 +81,44 @@ const Sidebar = ({isLarge , IsVideoPage } : any )=>{
     const {status } = useSession();
     const [subs,setSubs] = useState([]);
     const [loading,setLoading] = useState(true);
+    const [loading1,setLoading1] = useState(true);
+    const [list,setList] = useState([]);
+    const [show,setShow] = useState(false);
+    const playlists = async ()=>{
+      try{
+        const res = await fetch('/api/library/playlists',{ next : {revalidate : 300} , cache : 'default'});
+        const {data} = await res.json();
+        setList(data);
+        setLoading1(false);
+      }
+      
+      catch(err){
+        console.log(err);
+        setList([]);
+        setLoading1(false);
+      }
+    }
+
+    const fun = async ()=>{
+      try{
+        const res = await fetch('/api/subs',{ next : {revalidate : 300} , cache : 'default'});
+        const {subs,ptoken,ntoken} = await res.json();
+        setSubs(subs);
+        setLoading(false);
+      }
+
+      catch(err){
+        console.log(err);
+        setSubs([]);
+        setLoading(false);
+      }
+    }
+
 
     useEffect(()=>{
      if(status == 'authenticated'){
-        const fun = async ()=>{
-        try{
-          const res = await fetch('/api/subs',{ next : {revalidate : 300} , cache : 'default'});
-          const {subs,ptoken,ntoken} = await res.json();
-          setSubs(subs);
-          setLoading(false);
-        }
-  
-        catch(err){
-          console.log(err);
-          setSubs([]);
-          setLoading(false);
-        }
-      }
-
-      fun();
+        fun();
+        playlists();
      }
     },[status])
 
@@ -98,6 +135,38 @@ const Sidebar = ({isLarge , IsVideoPage } : any )=>{
             return <SideLinks key={index} index={index} item={item} isLarge={isLarge} />
           })
         }
+
+        { status == 'authenticated' &&
+          <>
+        <motion.button onClick={()=>{setShow(!show)}} className={`w-full dark:text-white hover:bg-[rgb(0,0,0,0.05)] dark:hover:bg-[rgb(255,255,255,0.05)] flex flex-nowrap items-center  ${!isLarge && 'justify-center flex-col-reverse mb-5'} p-[5%] mb-1 overflow-hidden rounded-xl font-[350] `}> 
+        <motion.div layout transition={{duration : 0.5}} > <AiOutlineDown/> </motion.div> 
+        <motion.div layout transition={{duration : 0.5}} className={ isLarge ? 'ml-5' :'mt-1 text-center text-xs' }>Show More</motion.div> 
+        </motion.button>
+         
+        { 
+        <motion.div className={`${!show && 'max-h-0'} overflow-y-hidden`}>
+          {loading1 ? <>
+          <SubsSkeleton isLarge={isLarge}/>
+          <SubsSkeleton isLarge={isLarge}/>
+          <SubsSkeleton isLarge={isLarge}/>
+          <SubsSkeleton isLarge={isLarge}/>
+          </> :
+        <>
+         {
+          list?.map((item : any , index : any)=>{
+              return <Link key={index} href={`/channel/${item?.snippet?.channelId}/playlist/${item?.id}`} className={`w-full ${ 'dark:text-white hover:bg-[rgb(0,0,0,0.05)] dark:hover:bg-[rgb(255,255,255,0.05)]'} flex flex-nowrap items-center  ${!isLarge && 'justify-center flex-col mb-5'} p-[5%] mb-1 overflow-hidden rounded-xl font-[350] `}> 
+                    <motion.div layout transition={{duration : 0.5}} > <RiPlayList2Line/> </motion.div> 
+                    <motion.div layout transition={{duration : 0.5}} className={ isLarge ? 'ml-5' :'mt-1 text-center text-xs' }>{item?.snippet?.title}</motion.div> 
+                    </Link>
+          })
+         }
+        </>}
+        </motion.div>
+        }
+        </>
+        }
+
+        
        
         <motion.hr layout transition={{duration : 0.5}} className={`m-auto border-0 h-[1px] mt-[7%] bg-white ${isLarge ? 'w-[85%]' : 'w-3/4 mb-10'}`}/>
     </motion.div>
@@ -128,22 +197,7 @@ const Sidebar = ({isLarge , IsVideoPage } : any )=>{
 export default Sidebar;
 
 
-const SubSkeleton = ({isLarge} : any)=>{
-   return <motion.div layout transition={{duration : 0.5}} className="subscribed-list w-full pl-[3%] pb-[15%]">
-   {isLarge && 
-    <motion.h3 layout transition={{duration : 0.5}} initial={{opacity : isLarge ? 0 : 1}} animate={{opacity : isLarge ? 1 : 0}} className="text-[18px] dark:text-white ml-2 my-5 text-[#5a5a5a]">Subscriptions</motion.h3>
-   }
 
-  <SubsSkeleton isLarge={isLarge}/>
-  <SubsSkeleton isLarge={isLarge}/>
-  <SubsSkeleton isLarge={isLarge}/>
-  <SubsSkeleton isLarge={isLarge}/>
-  <SubsSkeleton isLarge={isLarge}/>
-  <SubsSkeleton isLarge={isLarge}/>
-  <SubsSkeleton isLarge={isLarge}/>
-   
-</motion.div>
-}
 
 const SubsSkeleton = ({isLarge}:any)=>{
   return <motion.div layout transition={{duration : 0.5}} >
