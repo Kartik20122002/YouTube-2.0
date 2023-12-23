3d0305997244b3de72442b820e0ac85209e01c72
Kartik Hatwar <kartikhatwar98@gmail.com>
2023-12-23 19:59:26 +0530

    Improved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,28 @@
 'use client'
-import { signOut, useSession } from "next-auth/react"
-import {useContext, useEffect } from "react";
+import { useSession } from "next-auth/react"
+import { useContext } from "react";
 import { motion } from "framer-motion";
 import Loader from "@/components/global/loader/Loader";
 import { slideContext } from "./layout";
 import PageSection from "@/components/global/pagesection/pagesection";
 
-const dynamic = 'force-dynamic'
-
 
 export default function Home() {
- 
+
   const { status } = useSession();
 
-  const {slide , setslide} = useContext(slideContext) as any;
+  const { slide, setslide } = useContext(slideContext) as any;
   setslide(0);
 
- 
+
   return (
     <>
-          {status == 'loading' ?
-              <Loader/> :
-              <motion.div layout transition={{duration : 0.5}}>
-              <PageSection page={'popular'}/>
-              </motion.div>
-          }
+      {status == 'loading' ?
+        <Loader /> :
+        <motion.div layout transition={{ duration: 0.5 }}>
+          <PageSection page={'popular'} />
+        </motion.div>
+      }
     </>
   )
 }
