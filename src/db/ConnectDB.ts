a6ffc23bd40a29878ae9baa3385e0368d7766faf
Kartik Hatwar <kartikhatwar98@gmail.com>
2023-12-28 18:21:43 +0530

    update #8

--- src/db/ConnectDB.ts
+++ src/db/ConnectDB.ts
@@ -1,11 +1,8 @@
 import mongoose from 'mongoose';
-import { config } from 'dotenv'
-
-config();
 
 const ConnectDB = async () => {
     try {
-        const uri = process.env.MONGODB_URI as string
+        const uri = process.env.MONGODB_URI as string;
         const connection = await mongoose.connect(uri);
     } catch (error) {
         console.log(error);
