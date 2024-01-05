c2c1e9720a411619f47317ffe6f1444842c7252e
Kartik Hatwar <kartikhatwar98@gmail.com>
2024-01-05 22:37:45 +0530

    trying offline support

---
+++ _offline.tsx
@@ -0,0 +1,10 @@
+const OfflinePage = () => {
+    return <>
+        <div className="flex justify-center items-center dark:bg-black dark:text-white">
+            You are Offline.
+            Please Try Again.
+        </div>
+    </>
+}
+
+export default OfflinePage;
