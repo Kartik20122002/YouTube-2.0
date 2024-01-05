d866b8b13c3c87b791d4e972030c81a466eee169
Kartik Hatwar <kartikhatwar98@gmail.com>
2024-01-05 22:38:00 +0530

    asfd

--- src/app/_offline.js
+++ src/app/_offline.js
@@ -1,7 +1,8 @@
 const OfflinePage = ()=>{
     return <>
     <div className="flex justify-center items-center dark:bg-black dark:text-white">
-        You are Offline
+            You are Offline.
+            Please Try Again.
     </div>
     </>
 }
