81c3fb15f18a1b23e04e6a65226e11e49f99ec4d
Kartik Hatwar <kartikhatwar98@gmail.com>
2024-01-07 03:57:20 +0530

    skeleton ui improved

--- src/components/global/skeletonComponents/ImgSkeleton.tsx
+++ src/components/global/skeletonComponents/ImgSkeleton.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 
 const Sekelton = ({width, height, circle = false ,borderRadius = 0, className } : any)=>{
-   return <motion.div layout transition={{ duration: 0.5 }} className={`dark:bg-[#202324] bg-[#b8b8b8] animate-pulse ${width || 'min-w-full'} ${height || 'min-h-full'} ${className}  ${circle && '!rounded-full'} ${borderRadius && `rounded-[${borderRadius}]`}`}>
+   return <motion.div layout transition={{ duration: 0.5 }} className={`skeletonUi  ${width || 'min-w-full'} ${height || 'min-h-full'} ${className}  ${circle && '!rounded-full'} ${borderRadius && `rounded-[${borderRadius}]`}`}>
    </motion.div>
 }
 
--- src/components/global/skeletonComponents/TextSkeleton.tsx
+++ src/components/global/skeletonComponents/TextSkeleton.tsx
@@ -1,5 +1,5 @@
 const Sekelton = ({width, height, className } : any)=>{
-      return <div className={`dark:bg-[#202324] bg-[#b8b8b8] animate-pulse ${width || 'min-w-full'} ${height || 'min-h-[1rem]'} ${className} rounded-lg mb-[0.3rem]`}>
+      return <div className={`skeletonUi ${width || 'min-w-full'} ${height || 'min-h-[1rem]'} ${className} rounded-lg mb-[0.3rem]`}>
          </div>
  }
  
