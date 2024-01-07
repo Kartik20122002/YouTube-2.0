80153916d3e730c513a3b2d5b31788a7b4ccb5ea
Kartik Hatwar <kartikhatwar98@gmail.com>
2024-01-08 04:39:52 +0530

    fadsgdgf

---
+++ src/app/channel/[channelId]/loading.tsx
@@ -0,0 +1,100 @@
+import React, { useContext } from 'react';
+import { motion } from 'framer-motion';
+import { isLargeContext } from '@/app/layout';
+import SekeltonImg from '@/components/global/skeletonComponents/ImgSkeleton';
+import SekeltonText from '@/components/global/skeletonComponents/TextSkeleton';
+
+const ChannelPageSkeleton = () => {
+
+    return (<>
+        <motion.div layout transition={{ duration: 0.5 }} className='overflow-y-scroll h-screen pb-[10rem] dark:bg-black bg-white dark:text-white'>
+            <ChannelInfoSkeleton />
+            <VideoSectionSkeleton />
+            <VideoSectionSkeleton />
+        </motion.div>
+    </>)
+}
+
+const ChannelInfoSkeleton = () => {
+ 
+    return <>
+        <motion.div layout transition={{ duration: 0.5 }} className="flex relative flex-wrap dark:bg-black bg-white md:mt-6">
+
+            <motion.div layout transition={{ duration: 0.5 }} className="basis-full relative md:min-w-[15rem] h-full md:basis-[15%] md:px-8 ">
+                <motion.div layout transition={{ duration: 0.5 }} className="w-full h-full md:relative md:pb-[100%] pb-[40%] overflow-hidden">
+                    <motion.div layout transition={{ duration: 0.5 }} className="absolute w-full h-full top-0 md:right-0 md:bottom-0 md:left-0">
+                        <SekeltonImg className='md:!rounded-full' />
+                    </motion.div>
+                </motion.div>
+            </motion.div>
+
+
+            <motion.div layout transition={{ duration: 0.5 }} className="basis-[60%] shrink-0 mt-2 md:mt-0 flex min-w-min pl-3 md:pl-0 flex-col justify-center items-start grow">
+                <motion.div layout transition={{ duration: 0.5 }} className="text-[2rem] w-full"><SekeltonText height={'min-h-[2rem]'} width={'w-3/4'} /></motion.div>
+                 <SekeltonText />
+                <SekeltonText /> 
+            </motion.div>
+
+            <motion.div layout transition={{ duration: 0.5 }} className="flex items-center justify-end my-1 px-6 grow">
+                <SekeltonText width={'max-w-[7rem]'} height={'min-h-[2.3rem]'} className='w-[7rem] h-[2.3rem] !rounded-full cursor-pointer' />
+            </motion.div>
+        </motion.div>
+    </>
+}
+
+const VideoSectionSkeleton = () => {
+
+    return <>
+
+            <motion.hr layout transition={{ duration: 0.5 }} className='my-6 border-none h-[0.1px] bg-[#69696945]' />
+
+            <motion.div layout transition={{ duration: 0.5 }} className="md:mx-2 dark:bg-black bg-white">
+                 <motion.div layout transition={{ duration: 0.5 }} className='mb-5 mx-2 md:mx-0 flex justify-between'>
+                        <SekeltonText height={'min-h-[2rem]'} width={'w-[20%]'} className="min-w-[12rem]" />
+                        <SekeltonText height={'min-h-[2.5rem]'} width={'w-[6%]'} className="!rounded-full min-w-[4rem]" />
+                    </motion.div> 
+
+                <SkeletonGallery />
+
+            </motion.div>
+        </>
+        
+}
+
+const SkeletonGallery = () => {
+    const { isLarge } = useContext(isLargeContext) as any;
+    return <>
+        <motion.div layout transition={{ duration: 0.5 }} className="justify-evenly flex-wrap dark:bg-black bg-white flex">
+                <SkeletonCard index={0} />
+                <SkeletonCard index={1} />
+                <SkeletonCard index={2} />
+                {!isLarge && <>
+                <SkeletonCard index={3} />
+                <SkeletonCard index={4} />
+                <SkeletonCard index={5} />
+                </>
+                }
+        </motion.div>
+    </>
+}
+
+const SkeletonCard = ({ index }: any) => {
+    const { isLarge } = useContext(isLargeContext) as any;
+    return <>
+        <motion.div layout transition={{ duration: 0.5, delay: !isLarge ? (index % 10) / 10 : 0 }} className="flex flex-col md:mx-2 my-2 md:max-w-[20rem] min-w-6 w-full">
+            <motion.div layout transition={{ duration: 0.5 }} className="relative w-full h-full pt-[56.25%] overflow-hidden">
+                <motion.div layout transition={{ duration: 0.5 }} className="w-full h-full absolute top-0 right-0 left-0 bottom-0">
+                    <SekeltonImg className="md:!rounded-lg" />
+                </motion.div>
+            </motion.div>
+            <motion.div layout transition={{ duration: 0.5 }} className="mt-1 w-full">
+                <motion.div layout transition={{ duration: 0.5 }} className="truncate-2 text-[0.9rem] md:text-[1rem] whitespace-normal "><SekeltonText /></motion.div>
+                <motion.div layout transition={{ duration: 0.5 }} className="text-[#979696] w-1/2 text-[0.8rem] md:text-[1rem]"><SekeltonText width={'min-w-[50%]'} /></motion.div>
+            </motion.div>
+        </motion.div>
+    </>
+}
+
+
+export default ChannelPageSkeleton;
+
---
+++ src/components/library/loading.tsx
@@ -0,0 +1,89 @@
+import { motion } from "framer-motion";
+import SekeltonImg from "../global/skeletonComponents/ImgSkeleton";
+import SekeltonText from "../global/skeletonComponents/TextSkeleton";
+
+const LibraryPageSkeleton = () => {
+
+  return <>
+    <motion.div layout transition={{ duration: 0.5 }} className='md:pl-3 h-[90vh] pb-10 overflow-y-scroll flex flex-col-reverse md:flex-row dark:text-white'>
+      <motion.div layout transition={{ duration: 0.5 }} className="basis-[80%] grow">
+
+        <VideoSectionSkeleton />
+        <VideoSectionSkeleton />
+        <VideoSectionSkeleton />
+
+      </motion.div>
+
+      <UserDetailSkeleton />
+    </motion.div>
+  </>
+}
+
+const UserDetailSkeleton = () => {
+
+
+  return <motion.div layout transition={{ duration: 0.5 }} className="basis-[20%] min-h-max grow-0 items-center pt-[4rem] md:flex flex-col bg-[3red]">
+    <motion.div layout transition={{ duration: 0.5 }} className="flex flex-col items-center">
+      <motion.div layout transition={{ duration: 0.5 }} className="mb-2">
+          <SekeltonImg width="min-w-[80px] max-w-[80px]" height="min-h-[80px] max-h-[80px]" circle /> 
+      </motion.div>
+        <SekeltonText /> 
+    </motion.div>
+    <motion.div layout transition={{ duration: 0.5 }} className="my-7 w-full px-6">
+      <motion.hr className="mb-[0.6rem] border-none h-[0.07px] bg-[#5e5e5e36]" />
+      <motion.div layout transition={{ duration: 0.5 }} className="flex justify-between text-[0.75rem] font-[400] px-5 text-[#b2aca2]"><motion.span transition={{ duration: 0.5 }}>Subscriptions</motion.span> <motion.span transition={{ duration: 0.5 }} className="ml-2"></motion.span></motion.div>
+      <motion.hr className="my-[0.6rem] border-none h-[0.07px] bg-[#5e5e5e36]" />
+      <motion.div layout transition={{ duration: 0.5 }} className="flex justify-between text-[0.75rem] font-[400] px-5 text-[#b2aca2]"><motion.span transition={{ duration: 0.5 }}>Uploads</motion.span> <motion.span transition={{ duration: 0.5 }} className="ml-2"></motion.span></motion.div>
+      <motion.hr className="my-[0.6rem] border-none h-[0.07px] bg-[#5e5e5e36]" />
+      <motion.div layout transition={{ duration: 0.5 }} className="flex justify-between text-[0.75rem] font-[400] px-5 text-[#b2aca2]"><motion.span transition={{ duration: 0.5 }}>Views</motion.span> <motion.span transition={{ duration: 0.5 }} className="ml-2"></motion.span></motion.div>
+      <motion.hr className="my-[0.6rem] border-none h-[0.07px] bg-[#5e5e5e36]" />
+    </motion.div>
+  </motion.div>
+}
+
+const VideoSectionSkeleton = () => {
+
+  return <motion.div layout transition={{ duration: 0.5 }} className="min-h-max">
+          <motion.div layout transition={{ duration: 0.5 }} className='mb-5 mx-2 flex justify-between'>
+            <SekeltonText height={'min-h-[2rem]'} width={'w-[20%]'} className="min-w-[12rem]" />
+            <SekeltonText height={'min-h-[2.5rem]'} width={'w-[6%]'} className="!rounded-full min-w-[4rem]" />
+          </motion.div>
+
+        <motion.div layout transition={{ duration: 0.5 }} className={`flex w-screen md:w-full md:overflow-x-auto overflow-x-scroll md:justify-evenly md:flex-wrap`}>
+
+            <SkeletonCard />
+            <SkeletonCard />
+            <SkeletonCard />
+            <SkeletonCard />
+            <SkeletonCard />
+
+        </motion.div>
+
+        <motion.hr className="my-4 border-none h-[0.07px] bg-[#5e5e5e36]" />
+
+      </motion.div>
+    
+}
+
+const SkeletonCard = () => {
+
+  return <>
+
+    <motion.div layout transition={{ duration: 0.5 }} className="flex flex-col mx-4 md:mx-[0.1rem] my-2 max-w-[13rem] min-w-[13rem] w-[13rem]">
+      <motion.div layout transition={{ duration: 0.5 }} className="relative w-full pt-[56.25%] overflow-hidden">
+        <motion.div layout transition={{ duration: 0.5 }} className="w-full h-full absolute top-0 right-0 left-0 bottom-0">
+          <SekeltonImg className='!rounded-lg' />
+        </motion.div>
+      </motion.div>
+      <motion.div layout transition={{ duration: 0.5 }} className="mt-2 pr-6">
+        <motion.div layout transition={{ duration: 0.5 }} className="truncate-2 font-[650] text-[0.8rem] md:text-[0.9rem] whitespace-normal"><SekeltonText /></motion.div>
+        <motion.div layout transition={{ duration: 0.5 }} className="truncate-1 font-[550] text-grey text-[0.7rem] md:text-[0.8rem] whitespace-normal mt-2"><SekeltonText width="min-w-[50%] w-[50%]" /></motion.div>
+        <motion.div layout transition={{ duration: 0.5 }} className="text-grey font-[500] text-[0.5rem] md:text-[0.8rem]"> <SekeltonText width="min-w-[50%] w-[50%]" /></motion.div>
+      </motion.div>
+    </motion.div>
+
+  </>
+}
+
+
+export default LibraryPageSkeleton;
