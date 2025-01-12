b934ce3bee767d268bbfb065efaa781fdcacfe9c
Kartik Hatwar <92972026+Kartik20122002@users.noreply.github.com>
2025-01-12 13:37:33 +0530

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,6 @@ const LostPage = () => {
     return <div className="text-white w-full h-full flex flex-col text-lg dark:bg-black bg-white items-center justify-center">
         <FaYoutube className="text-[4rem] text-[red] mb-2" />
         Sorry , You are Lost.
-        Please Go Back.
     </div>
 }
 
